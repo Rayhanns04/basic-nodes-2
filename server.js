938796d08b57730e70bba54f89718a1f44a960a7
Muhammad Rayhan <muhrayhanns04@gmail.com>
2021-05-09 21:29:00 +0700

    better

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { PORT = 4000 } = process.env;
 
 console.log("PORT" + PORT);
 
-app.get("/", (req, res) => res.send("Hellow this is Node! Update-2"));
+app.get("/", (req, res) => res.send("Hellow this is Node! Update-3"));
 
 app.listen(PORT, () =>
   console.log(
