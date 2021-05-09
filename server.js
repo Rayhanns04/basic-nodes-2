171973e0e785ae3195d0f41c4895a14d937503df
Muhammad Rayhan <muhrayhanns04@gmail.com>
2021-05-09 21:31:20 +0700

    better

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const { PORT = 4000 } = process.env;
 
+const bodyParser = require("body-parser");
+const db = require("./models");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(bodyParser.json({ type: "application/json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
 console.log("PORT" + PORT);
 
 app.get("/", (req, res) => res.send("Hellow this is Node! Update-3"));
