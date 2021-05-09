713bc9cc8ab14aea35ba915ec6161ee46a3ba366
Muhammad Rayhan <muhrayhanns04@gmail.com>
2021-05-09 21:06:36 +0700

    better

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const { PORT = 3001 } = process.env;
+
+console.log("PORT" + PORT);
+
+app.get("/", (req, res) => res.send("Hellow this is Node!"));
+
+app.listen(PORT, () =>
+  console.log(
+    `Really basic nodes with app listening at http://locahost:${PORT}`
+  )
+);
